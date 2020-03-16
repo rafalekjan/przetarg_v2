ba0f5ee4672f8c9d79c50e2808abf814e9b845fc
rafalekjan <48844410+rafalekjan@users.noreply.github.com>
2020-03-16 15:40:16 +0100

    update
    execel issue is visible

--- general.py
+++ general.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
+import time
+from openpyxl import Workbook, load_workbook
 
 
 def otworz_strone_selenium(adres_www):
@@ -54,26 +56,89 @@ def adres_soap(podst_adres, strona, numer_obiektu, css, css_adres):
         return podst_adres
 
 
+def odczyt_selenium():
+    dane_strony = otworz_strone_selenium(www_strona)
+    ilosc_obiektow = ilosc_obiektow_selenium(dane_strony, css_strona)
+    for nr in range(ilosc_obiektow):
+        lista_tytulow.append(tytul_selenium(dane_strony, nr, css_strona, css_tytul))
+        lista_adresow.append(adres_selenium(base_url, dane_strony, nr, css_strona, css_adres))
 
+
+def odczyt_soap():
+    dane_strony = otworz_strone_soap(www_strona)
+    ilosc_obiektow = ilosc_obiektow_soap(dane_strony, css_strona)
+    for nr in range(ilosc_obiektow):
+        lista_tytulow.append(tytul_soap(dane_strony, nr, css_strona, css_tytul))
+        lista_adresow.append(adres_soap(base_url, dane_strony, nr, css_strona, css_adres))
+
+
+def odczyt_historii():
+    historia_tytulow = open("historia.txt", "r")
+    # print(historia_tytulow.readlines())
+    # return historia_tytulow.readlines()
+    return historia_tytulow.read()
+
+
+def zapis_historii(nr):
+    historia_tytulow = open("historia.txt", "a+")
+    historia_tytulow.write(lista_tytulow[nr] + '\n')
+    historia_tytulow.close()
+
+
+def zapis_raportu_txt(nr):
+    historia_tytulow = open("raport_" + aktualna_data + ".txt", "a+")
+    historia_tytulow.write(lista_tytulow[nr] + '\n')
+    historia_tytulow.close()
+
+
+def zapis_raportu(nr):
+    plik_raportu = load_workbook("Raport_" + aktualna_data + ".xlsx")
+    wyniki_raportu = plik_raportu.active
+    wyniki_raportu.cell(row=raport_kolumna, column=1).value = '=HYPERLINK("{}", "{}")'.format(lista_adresow[nr], ">Link<")
+    wyniki_raportu.cell(row=raport_kolumna, column=2).value = lista_tytulow[nr]
+    plik_raportu.save("Raport_" + aktualna_data + ".xlsx")
+
+
+def tworzenie_raportu():
+    try:
+        load_workbook("Raport_" + aktualna_data + ".xlsx")
+        raport_kolumna += 1
+    except:
+        plik_raportu = Workbook()
+        plik_raportu.save("Raport_" + aktualna_data + ".xlsx")
+        raport_kolumna = 1
+
+
+def sprawdzenie_historii():
+    for nr, tytul in enumerate(lista_tytulow):
+        if tytul not in odczyt_historii():
+            tworzenie_raportu()
+            zapis_historii(nr)
+            zapis_raportu(nr)
+
+
+def start_programu(tryb):
+    if tryb == "selenium":
+        odczyt_selenium()
+        sprawdzenie_historii()
+    if tryb == "soap":
+        odczyt_soap()
+        sprawdzenie_historii()
+
+
+aktualna_data = time.strftime("%Y_%m_%d")
+lista_tytulow = []
+lista_adresow = []
 www_strona = "https://bip.sobotka.pl/zamowienia/lista/13.dhtml"
 css_strona = "#lista_zamowien > tbody > tr"
 css_tytul = "td:nth-child(1) > a"
 # css_tytul = ""
 css_adres = "td:nth-child(1) > a"
 # css_adres = ""
-base_url = "www.dupa.com"
-# base_url = ""
+# base_url = "www.dupa.com"
+base_url = ""
 
-dane1 = otworz_strone_selenium(www_strona)
-dane2 = otworz_strone_soap(www_strona)
-ilosc_obiektow_1 = ilosc_obiektow_selenium(dane1, css_strona)
-ilosc_obiektow_2 = ilosc_obiektow_soap(dane2, css_strona)
-tytul1 = tytul_selenium(dane1, 0, css_strona, css_tytul)
-tytul2 = tytul_soap(dane2, 0, css_strona, css_tytul)
-adres1 = adres_selenium(base_url, dane1, 0, css_strona, css_adres)
-adres2 = adres_soap(base_url, dane2, 0, css_strona, css_adres)
-print(tytul1)
-print(adres1)
-print(tytul2)
-print(adres2)
 
+start_programu("selenium")
+
+# print(len(lista_adresow))
