6a5eb0056198bc898acd73680053c9b228922f3c
rafalekjan <48844410+rafalekjan@users.noreply.github.com>
2020-03-18 15:11:41 +0100

    update
    works properly

--- general.py
+++ general.py
@@ -36,8 +36,8 @@ def tytul_selenium(strona, numer_obiektu):
 def tytul_soup(strona, numer_obiektu):
     css_nth = aktualna_css_strona + ":nth-of-type(" + str(numer_obiektu + 1) + ")"
     if len(aktualna_css_tytul) > 0:
-        return strona.select(css_nth)[numer_obiektu].select(aktualna_css_tytul)[0].text.lower()
-    return strona.select(css_nth)[numer_obiektu].text.lower()
+        return strona.select(css_nth)[0].select(aktualna_css_tytul)[0].text.lower()
+    return strona.select(css_nth)[0].text.lower()
 
 
 def adres_selenium(strona, numer_obiektu):
@@ -67,19 +67,20 @@ def odczyt_selenium():
 def odczyt_soup():
     dane_strony = otworz_strone_soup()
     ilosc_obiektow = ilosc_obiektow_soup(dane_strony)
+    print("Znaleziono ogloszen:", ilosc_obiektow)
     for nr in range(ilosc_obiektow):
         lista_tytulow.append(tytul_soup(dane_strony, nr))
         lista_adresow.append(adres_soup(dane_strony, nr))
 
 
 def odczyt_historii():
-    historia_tytulow = open("historia.txt", "r")
+    historia_tytulow = open("historia.txt", "r+")
     return historia_tytulow.read()
 
 
 def zapis_historii(nr):
     historia_tytulow = open("historia.txt", "a+")
-    historia_tytulow.write(lista_tytulow[nr] + '\n')
+    historia_tytulow.write((lista_tytulow[nr]) + '\n')
     historia_tytulow.close()
 
 
@@ -92,7 +93,11 @@ def zapis_raportu_txt(nr):
 def zapis_raportu(nr):
     plik_raportu = load_workbook("Raport_" + aktualna_data + ".xlsx")
     wyniki_raportu = plik_raportu.active
-    wyniki_raportu.cell(row=len(wyniki_raportu["A"]) + 1, column=1).value = '=HYPERLINK("{}", "{}")'.format(lista_adresow[nr], ">Link<")
+    if len(lista_adresow[nr]) > 255:
+        wyniki_raportu.cell(row=len(wyniki_raportu["A"]) + 1, column=1).value = lista_adresow[nr]
+    else:
+        wyniki_raportu.cell(row=len(wyniki_raportu["A"]) + 1, column=1).value = '=HYPERLINK("{}", "{}")'.format(
+            lista_adresow[nr], ">Link<")
     wyniki_raportu.cell(row=len(wyniki_raportu["A"]), column=2).value = lista_tytulow[nr]
     plik_raportu.save("Raport_" + aktualna_data + ".xlsx")
 
@@ -130,7 +135,10 @@ def odczyt_danych_kolumna(plik_excel, kolumna):
     dane = []
     otwarty_excel = load_workbook(plik_excel).active
     for i in range(1, len(otwarty_excel["A"])):
-        dane.append(odczytaj_z_excela(otwarty_excel, i, kolumna))
+        if odczytaj_z_excela(otwarty_excel, i, kolumna) is not None:
+            dane.append(odczytaj_z_excela(otwarty_excel, i, kolumna))
+        else:
+            dane.append("")
     return dane
 
 
@@ -148,18 +156,30 @@ css_tytuly = odczyt_danych_kolumna("Baza_stron.xlsx", 8)
 css_adresy = odczyt_danych_kolumna("Baza_stron.xlsx", 7)
 podst_adresy = odczyt_danych_kolumna("Baza_stron.xlsx", 9)
 metody_www = odczyt_danych_kolumna("Baza_stron.xlsx", 6)
+gotowosc_raportowania = odczyt_danych_kolumna("Baza_stron.xlsx", 12)
 sprawdz_dane_excela()
 
 aktualna_data = time.strftime("%Y_%m_%d")
 lista_tytulow = []
 lista_adresow = []
 
-
-for i in range(1, 2):
-    aktualna_strona_www = strony_www[i]
-    aktualna_css_strona = css_strony[i]
-    aktualna_css_tytul = css_tytuly[i]
-    aktualna_css_adres = css_adresy[i]
-    aktualna_podst_adres = podst_adresy[i]
-    aktualna_metoda_www = metody_www[i]
-    start_programu()
+print(time.strftime("%T"))
+for i in range(1, 27):
+    print("Strona nr:", i)
+    aktualna_gotowosc_raportowania = gotowosc_raportowania[i]
+    if aktualna_gotowosc_raportowania == "TAK":
+        aktualna_strona_www = strony_www[i]
+        aktualna_css_strona = css_strony[i]
+        aktualna_css_tytul = css_tytuly[i]
+        aktualna_css_adres = css_adresy[i]
+        aktualna_podst_adres = podst_adresy[i]
+        aktualna_metoda_www = metody_www[i]
+        # print(aktualna_strona_www)
+        # print(aktualna_css_strona)
+        # print(aktualna_css_tytul)
+        # print(aktualna_css_adres)
+        # print(aktualna_podst_adres)
+        # print(aktualna_metoda_www)
+        # print(aktualna_gotowosc_raportowania)
+        start_programu()
+print(time.strftime("%T"))
